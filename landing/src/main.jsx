4586646a0b41c47d4f156c9d8bb94b4bcdfbb04a
gynsys <dramarielh@gmail.com>
2026-06-02 16:25:08 -0400

    Fix router basename and install Tailwind CSS

--- landing/src/main.jsx
+++ landing/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/arko360">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
