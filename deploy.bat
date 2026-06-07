4058453d198d8e2227050ff871c2bc6d1c9d5513
gynsys <dramarielh@gmail.com>
2026-06-07 17:30:24 -0400

    Lanzamiento MVP ARKO3D

--- deploy.bat
+++ deploy.bat
@@ -17,7 +17,7 @@ git push
 
 echo.
 echo [3/3] Actualizando y reconstruyendo contenedores en DigitalOcean...
-python ssh_runner.py "cd /var/www/arko360_platform && git pull origin main && docker compose pull && docker compose up -d --build"
+python ssh_runner.py "cd /var/www/arko360_platform && git pull origin main && docker compose pull && docker compose up -d --build && docker compose exec backend python init_db.py"
 
 echo.
 echo ==========================================
